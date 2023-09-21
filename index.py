3788877e5026328c9521feaecc5b9b8955c47166
Shuga <git@shuga.co>
2023-09-20 23:13:37 -0400

    autosudo

--- index.py
+++ index.py
@@ -201,7 +201,8 @@ async def oauth_transformer_new(
                 "last_name": hackucf_query[0].get("surname"),
                 "email": hackucf_query[0].get("email"),
                 "experience": int(hackucf_query[0].get("experience", 0)),
-                "hackucf_id": hackucf_query[0].get("id")
+                "hackucf_id": hackucf_query[0].get("id"),
+                "sudo": hackucf_query[0].get("id", False)
             }
             # table.update_item(
             #     Key={"id": query_for_id[0].get("id")},
